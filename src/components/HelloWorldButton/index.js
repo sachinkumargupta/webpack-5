7c677aee3cb269b13907be98224132cdfb9527a5
sachinkreeti <sgupta@kreeti.com>
2021-04-28 18:25:56 +0530

    Add babel loader for js files
    Babel presets has been set to @babel/env and all specifications supported in
    ECMAScript to ES5 which is supported by all major browsers.
    Class Properties are not included in the ECMAScript and so we added the
    extra plugin for that

--- src/components/HelloWorldButton/index.js
+++ src/components/HelloWorldButton/index.js
@@ -1,10 +1,16 @@
 import "./index.scss";
 
 class HelloWorldButton {
+  // in the tutorial, webpack version was throwing error in this line,
+  // reason webpack was not supportting class properties (attributes)
+  // so we needed a loader - babel
+  // right now even optional chaining is supported by webpack
+  classNameList = { button: "button-style" };
+
   render() {
     const body = document.querySelector("body");
     const button = document.createElement("button");
-    button.classList.add("button-style");
+    button.classList.add(this.classNameList?.button);
 
     button.onclick = function () {
       console.log("I got clicked");
