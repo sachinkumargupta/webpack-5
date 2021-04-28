983ffe838d7baa2b60b2ded2099e672b67e1a6e7
sachinkreeti <sgupta@kreeti.com>
2021-04-28 17:50:30 +0530

    add support for scss files
    npm packages requried are sass-loader and node-sass.
    node-sass is deprecated and should not be used, use dart-sass

--- src/components/HelloWorldButton/index.js
+++ src/components/HelloWorldButton/index.js
@@ -1,10 +1,11 @@
-import "./index.css";
+import "./index.scss";
 
 class HelloWorldButton {
   render() {
+    const body = document.querySelector("body");
     const button = document.createElement("button");
     button.classList.add("button-style");
-    const body = document.querySelector("body");
+
     button.onclick = function () {
       console.log("I got clicked");
       const p = document.createElement("p");
@@ -12,6 +13,7 @@ class HelloWorldButton {
       p.innerHTML = "Hello World Button Clicked";
       body.append(p);
     };
+
     button.innerHTML = "Hello World Button";
     body.append(button);
   }
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         test: /\.css/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.scss/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
 };
