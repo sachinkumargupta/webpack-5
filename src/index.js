8d87346e74f2fd3cc7d1ab6037fbde90d6608f32
sachinkreeti <sgupta@kreeti.com>
2021-04-29 07:49:47 +0530

    demostrate how all css is being stored in only one file - dist/styles.css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import HelloWorldButton from "./components/HelloWorldButton";
+import Header from "./components/header";
+
+const newHeader = new Header();
+newHeader.render();
 
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
