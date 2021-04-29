5e1e619bc5ac5c2977539320b055cede0c22575c
sachinkreeti <sgupta@kreeti.com>
2021-04-29 08:23:01 +0530

    generate html file automatically during build process and use it
    Bundle output filename changes and we don't want to manually update this in
    our HTML file, so we use HtmlWebpackPlugin which automatically generates
    a new html file with these file included in them.
    
    Also, the url added contains path specified in the publicPath option as a root path,
    so if publicPath is "dist", then the complete path will become "dist/styles.7843ea6b.css",
    but we require only "styles.7843ea6b.css" as index.html file is located at the same level,
    so we make publicPath as empty string

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/",
+    publicPath: "",
     // publicPath: "http://some-cdn-where-image-is-store/",
   },
   mode: "none",
@@ -59,5 +60,6 @@ module.exports = {
         path.join(process.cwd(), "build/**/*"),
       ],
     }),
+    new HtmlWebpackPlugin(),
   ],
 };
