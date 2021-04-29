6a5c2b5ee464058b33e620df85f183c1c42ab930
sachinkreeti <sgupta@kreeti.com>
2021-04-29 11:06:21 +0530

    configure webpack-dev-server for development environment

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,6 +11,12 @@ module.exports = {
     // publicPath: "http://some-cdn-where-image-is-store/",
   },
   mode: "development",
+  devServer: {
+    contentBase: path.resolve(__dirname, ".dist"),
+    index: "index.html",
+    port: 9000,
+    writeToDisk: true,
+  },
   module: {
     rules: [
       {
